3cf74de004c20307d2a1c3020120821d48ea7fbc
Bahadır Akın Akgül <akinakgul@gmail.com>
2020-06-28 16:53:41 +0300

    Order service is created.

--- src/main/java/com/akgul/kangroo/pouch/service/OrderService.java
+++ src/main/java/com/akgul/kangroo/pouch/service/OrderService.java
@@ -6,26 +6,56 @@ import com.akgul.kangroo.pouch.entity.Order;
 import com.akgul.kangroo.pouch.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class OrderService {
+    private static final BigDecimal MINIMUM_ORDER_AMOUNT = BigDecimal.valueOf(0.01);
 
     @Autowired
     private OrderController orderController;
 
     public Order createOrder(Cart cart, User user) {
-       return null;
+        if (ObjectUtils.isEmpty(cart) || ObjectUtils.isEmpty(cart.getCartItems()) || ObjectUtils.isEmpty(user)) {
+            return null;
+        }
+
+        if (!isMinimumOrderAmountValid(cart)) {
+            return null;
+        }
+
+        if (!isOrderDeliveryOnFuture(cart)) {
+            return null;
+        }
+
+        Order order = new Order();
+        order.setCart(cart);
+        order.setTotalAmount(cart.getTotalAmount());
+        order.setUser(user);
+        order.setSelectedDeliveryDate(cart.getSelectedDeliveryDate());
+
+        orderController.saveOrder(order);
+        return order;
+    }
+
+    protected boolean isMinimumOrderAmountValid(Cart cart) {
+        return cart.getTotalAmount().compareTo(MINIMUM_ORDER_AMOUNT) >= 0;
+    }
+
+    protected boolean isOrderDeliveryOnFuture(Cart cart) {
+        return cart.getSelectedDeliveryDate().before(Date.valueOf(LocalDate.now()));
     }
 
     public Order getUserOrderById(User user, Long orderId) {
-        return null;
+        return orderController.getUserOrderById(user, orderId);
     }
 
     public List<Order> getOrdersByUser(User user) {
-        return null;
+        return orderController.getOrdersByUser(user);
     }
-
-
 }
---
+++ src/test/java/com/akgul/kangroo/pouch/service/OrderServiceTest.java
@@ -0,0 +1,130 @@
+package com.akgul.kangroo.pouch.service;
+
+import com.akgul.kangroo.pouch.controller.OrderController;
+import com.akgul.kangroo.pouch.entity.Cart;
+import com.akgul.kangroo.pouch.entity.CartItem;
+import com.akgul.kangroo.pouch.entity.Order;
+import com.akgul.kangroo.pouch.entity.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OrderServiceTest {
+
+    @InjectMocks
+    private OrderService orderService;
+
+    @Mock
+    private OrderController orderController;
+
+    @Test
+    public void shouldCheckMinimumOrderAmountIsValid() {
+        Cart cart = new Cart();
+        cart.setTotalAmount(BigDecimal.ZERO);
+        Assert.assertFalse(orderService.isMinimumOrderAmountValid(cart));
+    }
+
+    @Test
+    public void shouldNotCreateOrderWhenNoCartItemAvailable() {
+        Cart cart = new Cart();
+
+        User user = new User();
+        user.setUserName("user");
+
+        cart.setUser(user);
+        Assert.assertNull(orderService.createOrder(cart, user));
+    }
+
+    @Test
+    public void shouldNotCreateOrderWhenOrderDeliveryDateIsOnPast() {
+        Cart cart = new Cart();
+
+        User user = new User();
+        user.setUserName("user");
+
+        cart.setUser(user);
+        cart.setSelectedDeliveryDate(Date.valueOf(LocalDate.MIN));
+
+        cart.setCartItems(Arrays.asList(new CartItem()));
+
+        Assert.assertNull(orderService.createOrder(cart, user));
+    }
+
+    @Test
+    public void shouldNotCreateOrderWhenCartIsNotAvailable() {
+        Assert.assertNull(orderService.createOrder(null, new User()));
+    }
+
+    @Test
+    public void shouldNotCreateOrderWhenUserIsNotAvailable() {
+        Assert.assertNull(orderService.createOrder(new Cart(), null));
+    }
+
+    @Test
+    public void shouldCreateOrder() {
+        User user = new User();
+
+        Cart cart = new Cart();
+        cart.setTotalAmount(BigDecimal.TEN);
+        cart.setSelectedDeliveryDate(Date.valueOf(LocalDate.MAX));
+        cart.setUser(user);
+        cart.setCartItems(Arrays.asList(new CartItem()));
+
+        Order expectedOrder = new Order();
+        expectedOrder.setUser(user);
+        expectedOrder.setCart(cart);
+        expectedOrder.setTotalAmount(BigDecimal.TEN);
+        expectedOrder.setSelectedDeliveryDate(Date.valueOf(LocalDate.MAX));
+
+        when(orderController.saveOrder(any())).thenReturn(expectedOrder);
+
+        Order order = orderService.createOrder(cart, user);
+
+        Assert.assertEquals(order.getCart(), expectedOrder.getCart());
+    }
+
+    @Test
+    public void shouldGetUserOrderById() {
+        Order order = new Order();
+        User user = new User();
+        user.setUserName("test");
+        order.setUser(user);
+        order.setId(1L);
+
+        Mockito.when(orderController.getUserOrderById(user, 1L)).thenReturn(order);
+
+        Assert.assertEquals(order, orderService.getUserOrderById(user, 1L));
+    }
+
+    @Test
+    public void shouldGetOrdersByUser() {
+        User user = new User();
+        user.setUserName("test");
+
+        Order order1 = new Order();
+        order1.setUser(user);
+        order1.setId(1L);
+
+        Order order2 = new Order();
+        order2.setUser(user);
+        order2.setId(2L);
+
+        Mockito.when(orderController.getOrdersByUser(user)).thenReturn(Arrays.asList(order1, order2));
+
+        Assert.assertEquals(2, orderService.getOrdersByUser(user).size());
+    }
+
+}
