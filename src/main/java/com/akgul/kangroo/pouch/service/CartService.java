2bdf37942b17454021311c48b52d332b5bffac4c
Bahadır Akın Akgül <akinakgul@gmail.com>
2020-06-28 15:38:21 +0300

    Cart service is created.

--- src/main/java/com/akgul/kangroo/pouch/service/CartService.java
+++ src/main/java/com/akgul/kangroo/pouch/service/CartService.java
@@ -2,10 +2,14 @@ package com.akgul.kangroo.pouch.service;
 
 import com.akgul.kangroo.pouch.controller.CartController;
 import com.akgul.kangroo.pouch.entity.Cart;
+import com.akgul.kangroo.pouch.entity.CartItem;
 import com.akgul.kangroo.pouch.entity.Product;
 import com.akgul.kangroo.pouch.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CartService {
@@ -14,19 +18,31 @@ public class CartService {
     private CartController cartController;
 
     public Cart createCart(User user, Product product, int purchaseCount) {
-        return null;
+        Cart cart = new Cart();
+        List<CartItem> cartItems = new ArrayList<>();
+
+        CartItem cartItem = new CartItem();
+        cartItem.setCart(cart);
+        cartItem.setQuantity(purchaseCount);
+        cartItem.setProduct(product);
+
+        cartItems.add(cartItem);
+
+        cart.setCartItems(cartItems);
+        cart.setUser(user);
+        return saveCart(cart);
     }
 
     public Cart getCartByUser(User user) {
-        return null;
+        return cartController.getCartByUser(user);
     }
 
     public Cart saveCart(Cart cart) {
-       return null;
+       return cartController.saveCart(cart);
     }
 
     public Cart deleteCart(Cart cart) {
-       return null;
+        cart.setDeleted(true);
+        return cartController.saveCart(cart);
     }
-
 }
