18263aafb811175b5f82c74c695df21150c262d1
Bahadır Akın Akgül <akinakgul@gmail.com>
2020-06-29 13:05:21 +0300

    Swagger and Spring Security support have been added with Cart API service method templates.

---
+++ src/main/java/com/akgul/kangroo/pouch/api/controller/CartApiController.java
@@ -0,0 +1,60 @@
+package com.akgul.kangroo.pouch.api.controller;
+
+import com.akgul.kangroo.pouch.api.model.PouchApiResponse;
+import com.akgul.kangroo.pouch.service.CartItemService;
+import com.akgul.kangroo.pouch.service.CartService;
+import com.akgul.kangroo.pouch.service.ProductService;
+import com.akgul.kangroo.pouch.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cart")
+public class CartApiController {
+    private Logger LOGGER = LoggerFactory.getLogger(CartApiController.class);
+
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private CartItemService cartItemService;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
+    public @ResponseBody PouchApiResponse getCart(Authentication authentication) {
+        return null;
+    }
+
+    @RequestMapping(value = "/item/{cartItemId}", method = RequestMethod.GET)
+    public @ResponseBody PouchApiResponse getCartItemById(Authentication authentication, @PathVariable(value = "cartItemId") Long cartItemId) {
+       return null;
+    }
+
+    @RequestMapping(value = "/add/{productId}/{purchaseCount}", method = RequestMethod.POST)
+    public @ResponseBody PouchApiResponse addProductToCart(Authentication authentication, @PathVariable(value = "drinkProductId") Long productId, @PathVariable(value = "purchaseCount") int purchaseCount) {
+        return null;
+    }
+
+    @RequestMapping(value = "/update/{productId}/{cartItemId}/{purchaseCount}", method = RequestMethod.PUT)
+    public @ResponseBody PouchApiResponse updateProductQuantity(Authentication authentication, @PathVariable(value = "productId") Long productId, @PathVariable(value = "cartItemId") Long cartItemId, @PathVariable(value = "purchaseCount") int purchaseCount) {
+        return null;
+    }
+
+    @RequestMapping(value = "/delete/item/{cartItemId}", method = RequestMethod.DELETE)
+    public @ResponseBody PouchApiResponse deleteCartItem(Authentication authentication, @PathVariable(value = "cartItemId") Long cartItemId) {
+       return null;
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public @ResponseBody PouchApiResponse deleteCart(Authentication authentication) {
+        return null;
+    }
+}
