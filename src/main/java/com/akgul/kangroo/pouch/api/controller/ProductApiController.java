c998e185bb53ec2bbaadfc06b3c0c51500aaf777
Bahadır Akın Akgül <akinakgul@gmail.com>
2020-06-29 16:15:52 +0300

    Product API services are created.

--- src/main/java/com/akgul/kangroo/pouch/api/controller/ProductApiController.java
+++ src/main/java/com/akgul/kangroo/pouch/api/controller/ProductApiController.java
@@ -1,6 +1,9 @@
 package com.akgul.kangroo.pouch.api.controller;
 
+import com.akgul.kangroo.pouch.api.model.NotFoundPouchApiResponse;
+import com.akgul.kangroo.pouch.api.model.PouchApiResponse;
 import com.akgul.kangroo.pouch.api.model.ProductApiResponse;
+import com.akgul.kangroo.pouch.entity.Product;
 import com.akgul.kangroo.pouch.service.CartItemService;
 import com.akgul.kangroo.pouch.service.CartService;
 import com.akgul.kangroo.pouch.service.ProductService;
@@ -9,8 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @PreAuthorize("hasRole('CUSTOMER') or hasRole('ADMIN')")
@@ -22,22 +27,23 @@ public class ProductApiController {
     @Autowired
     private ProductService productService;
 
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private CartItemService cartItemService;
-
-    @Autowired
-    private CartService cartService;
-
     @RequestMapping(value = {"", "/", "/list"}, method = RequestMethod.GET)
     public @ResponseBody List<ProductApiResponse> getProductList() {
-        return null;
+        List<ProductApiResponse> productApiResponses = new ArrayList<>();
+        for (Product product : productService.getProducts()) {
+            productApiResponses.add(new ProductApiResponse(product));
+        }
+        return productApiResponses;
     }
 
     @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
-    public ProductApiResponse getProduct(@PathVariable(value = "productId") Long productId) {
-        return null;
+    public PouchApiResponse getProduct(@PathVariable(value = "productId") Long productId) {
+        Product product = productService.getProductById(productId);
+
+        if (ObjectUtils.isEmpty(product)) {
+            return new NotFoundPouchApiResponse("Product id " + productId + " not found");
+        }
+
+        return new ProductApiResponse(product);
     }
 }
